0ca104285b05041e10e2ba7228e7e8b8ed947a41
concreted <arichuang@gmail.com>
2014-06-30 20:27:33 -0700

    007

--- 007.py
+++ 007.py
@@ -7,5 +7,24 @@ that the 6^th prime is 13.
 
 What is the 10001^st prime number?
 """
+import math
+
+def isPrime(x):
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
+
+primeCount = 0
+i = 2
+howMany = 10001
+
+while primeCount < howMany:
+    if isPrime(i):
+        primeCount += 1
+    i += 1
+
+print i-1
+
 
 
