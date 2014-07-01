47d8e97a7e43fd6a108a132fb66809fd151221d8
concreted <arichuang@gmail.com>
2014-06-30 19:31:46 -0700

    006

--- 006.py
+++ 006.py
@@ -14,5 +14,15 @@ natural numbers and the square of the sum is 3025 385 = 2640.
 Find the difference between the sum of the squares of the first one
 hundred natural numbers and the square of the sum.
 """
+maximum = 100
 
+numrange = range(1, maximum+1)
+
+sum_of_squares = reduce(lambda x, y: x + y, map(lambda x: x*x, numrange))
+
+square_of_sums = reduce(lambda x, y: x + y, numrange) ** 2
+
+#print sum_of_squares
+#print square_of_sums
+print square_of_sums - sum_of_squares
 
