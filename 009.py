cfb6c11057a1114c94eed97e120d1812cb3e98ec
concreted <arichuang@gmail.com>
2014-07-03 22:50:36 -0700

    009 - suboptimal

--- 009.py
+++ 009.py
@@ -12,4 +12,28 @@ There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
 """
 
+triplesum = 1000
+c = 3
+b = 2
+a = 1
 
+done = False
+
+while a < triplesum:
+    #print a,b,c
+    if a*a + b*b == c*c and a + b + c == triplesum:
+        print a*b*c
+        quit()
+    else:
+        c += 1
+        if a+b+c > triplesum:
+            b += 1
+            c = b + 1
+            if a+b+c > triplesum:
+                a += 1
+                b = a+1
+                c = b+1
+    #inp = raw_input()
+
+print "failed!"
+print a*b*c
