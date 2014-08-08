4853119b4486cb3036ee660f95227fad6c269dd2
concreted <arichuang@gmail.com>
2014-08-07 19:40:13 -0700

    015 - fast solution w/ dynamic programming

--- 015.py
+++ 015.py
@@ -20,8 +20,30 @@ def findPaths(grid_size):
             return walkGrid(x+1, y) + walkGrid(x, y+1)
     return walkGrid(0,0)
 
+def findPathsDP(grid_size):
+    grid = []
+    for i in range(grid_size):
+        grid.append([0] * (grid_size) + [1])
+    grid.append([1] * (grid_size+1))
+    '''
+    for row in grid:
+        print row
+    '''
+    for i in range(grid_size-1, -1, -1):
+        for j in range(grid_size-1, -1, -1):
+            grid[i][j] = grid[i+1][j] + grid[i][j+1]
+    '''
+    print
+    for row in grid:
+        print row
+    '''
+    return grid[0][0]
+    
+print findPathsDP(20)
+'''
 print findPaths(1) == 2
 print findPaths(2) == 6
 print findPaths(3) == 20
 print findPaths(4) == 70
 print findPaths(5) == 252
+'''
